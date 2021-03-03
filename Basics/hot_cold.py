c584daaaf1252242dc1e1c7f9c326d516d0abc68
PreranaPandit <preranapandit07@gmail.com>
2021-03-03 14:31:39 +0545

    Initial commit

---
+++ Basics/hot_cold.py
@@ -0,0 +1,16 @@
+'''
+ if , else-if, else condition
+'''
+
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+#use shift + tab
+elif is_cold:
+    print("It's a cold day")
+    print("Wear a warm clothes")
+else:
+    print("It's a lovely day")
