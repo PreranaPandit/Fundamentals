35509c78e5dc3ba6e2c612633c13abf9ff1eed08
PreranaPandit <preranapandit07@gmail.com>
2021-03-05 15:57:48 +0545

    while loop

---
+++ Basics/q5.py
@@ -0,0 +1,25 @@
+'''
+  while condition
+    -- use block of codes multiple times
+    for making an interactive programs
+'''
+
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+print("Done")
+
+
+'''
+  * triangle shape using while loop 
+'''
+
+i = 1
+while i <= 5:
+    print('*' * i)
+    i = i + 1
+print("\nTriangle displayed")
+
+
+
