4a8519e66cdf100dfe3455903e36e8b01aa16231
PreranaPandit <preranapandit07@gmail.com>
2021-03-12 11:49:55 +0545

    nested loop uses

---
+++ Basics/q9.py
@@ -0,0 +1,36 @@
+'''
+ Question -
+       XXXXX
+       XX
+       XXXXX
+       XX
+       XX
+
+
+Create F using nested loops.
+'''
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print('X' * x_count)
+
+print('\n')
+
+'''
+F printing using while loop
+'''
+number = [5,2,5,2,2,2]
+for xCount in number:
+    output = ''
+    for count in range(xCount):
+        output += 'X'
+    print(output + '\n')
+
+'''
+L format display
+'''
+
+numbers = [2, 2, 2, 2, 5]
+for x_count in numbers:
+    print('X' * x_count)
+
